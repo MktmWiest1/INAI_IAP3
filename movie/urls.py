d57a1d983e3369843653a4cd467dc7aa41841dbf
mktmb <mktmboru@gmail.com>
2022-12-02 11:11:07 +0600

    second commit

--- movie/urls.py
+++ movie/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('movie/', views.movie_list, name="movie"),
     path('movie/<int:id>/', views.movie_detail, name="movie_id"),
+    path('movie/<int:id>/update/', views.movie_update, name='movie_update'),
+    path('movie/<int:id>/delete/', views.movie_delete, name='movie_delete'),
+    path('add-movie/', views.add_movie, name='add_movie')
 ]
 
--- movie/views.py
+++ movie/views.py
@@ -1,6 +1,6 @@
-
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-from movie import models
+from movie import models, forms
 
 
 def movie_list(request):
@@ -12,3 +12,33 @@ def movie_detail(request, id):
     movie_id = get_object_or_404(models.Movie, id=id)
     return render(request, 'movie_detail.html', {'movie_id': movie_id})
 
+
+def add_movie(request):
+    method = request.method
+    if method == 'POST':
+        form = forms.MovieForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Movie created')
+    else:
+        form = forms.MovieForm()
+    return render(request, 'add_movie.html', {'form': form})
+
+
+def movie_update(request, id):
+    movie_object = get_object_or_404(models.Movie, id=id)
+    if request.method == 'POST':
+        form = forms.MovieForm(instance=movie_object, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Movie Updated Successfully')
+
+    else:
+        form = forms.MovieForm(instance=movie_object)
+    return render(request, 'movie_update.html', {'form': form, 'object': movie_object})
+
+
+def movie_delete(request, id):
+    movie_object = get_object_or_404(models.Movie, id=id)
+    movie_object.delete()
+    return HttpResponse('Book Deleted')
